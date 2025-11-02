bcea7ea00c87527e0e2c06e698fc07190c366f2d
herman-xphp <hermanxphp@gmail.com>
2025-11-02 09:26:14 +0800

    custom validation parameter

--- validation_test.go
+++ validation_test.go
@@ -2,6 +2,8 @@ package belajar_golang_validation
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -394,5 +396,41 @@ func TestCustomValidationFunction(t *testing.T) {
 	err := validate.Struct(request)
 	if err != nil {
 		fmt.Println(err.Error())
+	}
+}
+
+var regexNumber = regexp.MustCompile("^[0-9]+$")
+
+func MustValidPin(field validator.FieldLevel) bool {
+	length, err := strconv.Atoi(field.Param())
+	if err != nil {
+		panic(err)
+	}
+
+	value := field.Field().String()
+	if !regexNumber.MatchString(value) {
+		return false
+	}
+
+	return len(value) == length
+}
+
+func TestCustomValidationParameter(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("pin", MustValidPin)
+
+	type Login struct {
+		Phone string `validate:"required,number"`
+		Pin   string `validate:"required,pin=6"`
+	}
+
+	request := Login{
+		Phone: "09493939392",
+		Pin:   "121212",
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
